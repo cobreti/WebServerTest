bd67b3b5c261dfbb1934ddf7f36bd6172ec26658
Danny Thibaudeau <cobreti_@hotmail.com>
2013-04-20 12:33:53 -0400

    Initial Commit

---
+++ WebServerTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WebServerTest
+//
+//  Created by Danny Thibaudeau on 2013-04-20.
+//  Copyright (c) 2013 Danny Thibaudeau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
